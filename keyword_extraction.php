84ea7703a538c0cb3fa7650deb625b4e222a052e
Sander Breukink <sander.breukink@optiply.nl>
2017-01-19 00:22:12 +0100

    Added keywords from database selection

--- keyword_extraction.php
+++ keyword_extraction.php
@@ -12,13 +12,26 @@ class keyword_extraction {
         //one update for everything, no splitting up for as long as this completes in reasonable amount of time.
         global $sql;
         global $config;
+        $return = array();
+
         $articles = $sql->fetch_object("
             SELECT * FROM " . $config->dbprefix . "articles 
             WHERE 
             content_text != ''  AND 
             (SELECT COUNT(*) FROM " . $config->dbprefix . "article_keywords WHERE " . $config->dbprefix . "article_keywords.article_id = " . $config->dbprefix . "articles.id) = 0");
 
-        $return = array("todo");
+        $return["articles"] = count($articles);
+        $stopwords = $sql->fetch_object("SELECT * FROM " . $config->dbprefix . "stop_words ");
+        $stopwordsarray = array();
+        foreach($stopwords as $word){
+            $stopwordsarray[] = $word->varchar;
+        }
+
+        $return["keywords"] = 0;
+
+        foreach($articles as $article){
+            //$keywords =
+        }
         return $return;
     }
 }
